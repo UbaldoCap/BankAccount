dedb6f99761501cfe3b04a16b8b157b50c548690
Ubaldo <ubaldocapalbo@gmail.com>
2023-11-21 19:45:33 +0100

    end

--- Main.java
+++ Main.java
@@ -1,13 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        ContoBancario conto1 = new ContoBancario("IT19388", 2000);
-        ContoBancario conto2 = new ContoBancario("IT19879", 200);
-        ContoBancario conto3 = new ContoBancario("IT14823", 1500);
-        ContoBancario conto4 = new ContoBancario("IT98754", 75);
-        Banca banca = new Banca("BNP", new ContoBancario[]{conto1}, 3);
-        banca.aggiungiConto(conto2);
-        banca.aggiungiConto(conto3);
-        banca.aggiungiConto(conto4);
+        //ContoBancario conto1 = new ContoBancario("IT19388", 2000);
+        //ContoBancario conto2 = new ContoBancario("IT19879", 200);
+        //ContoBancario conto3 = new ContoBancario("IT14823", 1500);
+        //ContoBancario conto4 = new ContoBancario("IT98754", 75);
+        Banca banca = new Banca("BNP", new ContoBancario[] {}, 3);
+        banca.aggiungiConto(new ContoBancario("IT19388", 2000));
+        banca.aggiungiConto(new ContoBancario("IT19879", 200));
+        banca.aggiungiConto(new ContoBancario("IT14823", 1500));
+        banca.aggiungiConto(new ContoBancario("IT98754", 75));
         banca.visualizzaInformazioneConti();
         banca.saldoTotale();
     }
